f66054e113384d0241661032e8b0789b2f271cbb
Fernando A. Garcia <fagarcia.ec.br@gmail.com>
2016-08-19 01:01:02 +0000

    Upload do conteúdo inicial - Ionic Creator

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('app.controllers', [])
+  
+.controller('quemVaiParticiparCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pegadorDaRodadaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('histRicoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pGinaInicialCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,41 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('quemVaiParticipar', {
+    url: '/page1',
+    templateUrl: 'templates/quemVaiParticipar.html',
+    controller: 'quemVaiParticiparCtrl'
+  })
+
+  .state('pegadorDaRodada', {
+    url: '/page3',
+    templateUrl: 'templates/pegadorDaRodada.html',
+    controller: 'pegadorDaRodadaCtrl'
+  })
+
+  .state('histRico', {
+    url: '/page4',
+    templateUrl: 'templates/histRico.html',
+    controller: 'histRicoCtrl'
+  })
+
+  .state('pGinaInicial', {
+    url: '/page5',
+    templateUrl: 'templates/pGinaInicial.html',
+    controller: 'pGinaInicialCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+  
+
+});
